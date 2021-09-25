d1c238258e5e3a9c4b21711298a5804e9f094765
Alina <alinchik-95@mail.ru>
2021-09-25 13:36:25 +0300

    first lesson completed

---
+++ src/main/java/lesson1.java
@@ -0,0 +1,9 @@
+public class lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Hello,world");
+    }
+
+    int a = 8;
+    float b = 8.8f;
+    String c = "Hi, Ivan";
+}
